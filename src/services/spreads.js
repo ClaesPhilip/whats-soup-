c850d293f2c625fab32739fb39adda5e15da1d21
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-10-22 23:15:49 +0200

    Create spreads.js

---
+++ src/services/spreads.js
@@ -0,0 +1,26 @@
+export const spreadProducts = [
+    {
+      id: 15,
+      name: "Whipped butter - with lemon and sea salt - 50 gram",
+      price: 15,
+      photo: "/img/image29.png",
+    },
+    {
+      id: 16,
+      name: "Beetroot Hummus - Traditional Hummus",
+      price: 15,
+      photo: "/img/image38.png"
+    },
+    {
+      id: 17,
+      name: "Truffle Aioli - Organic Aioli without Egg - 75 gram",
+      price: 15,
+      photo: "/img/image39.png"
+    },
+    {
+      id: 18,
+      name: "Sliced Avocado - 75 gram",
+      price: 15,
+      photo: "/img/image40.png"
+    },
+  ];
