a3299ed4912d719d75727110286c59cdfaf1b13a
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-10-22 23:15:41 +0200

    Create beverages.js

---
+++ src/services/beverages.js
@@ -0,0 +1,50 @@
+export const beveragesProducts = [
+    {
+      id: 21,
+      name: "Lemonade - Lime And Lemon ",
+      price: 25,
+      photo: "/img/image62.png",
+    },
+    {
+      id: 22,
+      name: "Juice - Blood Ornage",
+      price: 25,
+      photo: "/img/image60.png"
+    },
+    {
+      id: 23,
+      name: "Juice - Apple And Ginger",
+      price: 25,
+      photo: "/img/image61.png"
+    },
+    {
+      id: 24,
+      name: "Tree Shots - Different Taste every time",
+      price: 25,
+      photo: "/img/image63.png"
+    },
+    {
+      id: 25,
+      name: "Kombucha - Rasberry And Lemon Thyme",
+      price: 25,
+      photo: "/img/image58.png"
+    },
+    {
+      id: 26,
+      name: "Sparkling Water - just water - (H2O)",
+      price: 25,
+      photo: "/img/image65.png"
+    },
+    {
+      id: 27,
+      name: "Fritz-Kola",
+      price: 25,
+      photo: "/img/image54.png"
+    },
+    {
+      id: 28,
+      name: "SanPellegrino - Blood Orange",
+      price: 25,
+      photo: "/img/image45.png"
+      },
+  ];
