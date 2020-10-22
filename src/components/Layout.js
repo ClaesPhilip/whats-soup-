e462ca34aa2b79e3e61e45e5e3cf099ae3838cc2
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-10-22 23:14:13 +0200

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './shared/header';
+import Footer from './shared/footer';
+
+import { Helmet } from 'react-helmet-async';
+
+import 'bootswatch/dist/lux/bootstrap.css'
+
+const Layout = ({title, description, children}) => {
+    return ( 
+        <>
+        <Helmet>
+            <title>{ title ? title + " - What´s Soup" : "React.js Boilerplate" }</title>
+            <meta name = "description" content={ description || "Soup is good" } />
+        </Helmet>
+        <Header/>
+        <main className="container">
+            {children}
+        </main>
+        <Footer/>
+        </>
+     );
+}
+ 
+export default Layout;
