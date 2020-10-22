101dd55393c5f10b2729597c7bb795a1c5d79892
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-10-22 23:15:45 +0200

    Create protein.js

---
+++ src/services/protein.js
@@ -0,0 +1,26 @@
+export const dummyProducts = [
+    {
+      id: 6,
+      name: "Fried Tofu - Organic 100 gram, rich in quality protein",
+      price: 30,
+      photo: "/img/image26.png",
+    },
+    {
+      id: 7,
+      name: "Grilled Chicken - from Danish Family Farms - 100 gram",
+      price: 30,
+      photo: "/img/image36.png"
+    },
+    {
+      id: 8,
+      name: "Hand-peeled Shripms - Fresh from Norway - 100 gram",
+      price: 30,
+      photo: "/img/image37.png"
+    },
+    {
+      id: 9,
+      name: "Grilled Beef - 100 gram",
+      price: 30,
+      photo: "/img/image28.png"
+    },
+  ];
