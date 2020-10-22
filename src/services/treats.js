07670af87fa6f090aeaee4fb5a4ff46d1c5a6710
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-10-22 23:15:53 +0200

    Create treats.js

---
+++ src/services/treats.js
@@ -0,0 +1,14 @@
+export const treatsProducts = [
+    {
+      id: 19,
+      name: "American Pancakes - with lemon and blueberrys",
+      price: 15,
+      photo: "/img/image41.png",
+    },
+    {
+      id: 20,
+      name: "Pancakes - with cream and jam - perfect taste",
+      price: 15,
+      photo: "/img/image42.png"
+    },
+  ];
