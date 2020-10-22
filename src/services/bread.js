5e5a93a4c23beaf9306f82258345b4ba96759545
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-10-22 23:15:43 +0200

    Create bread.js

---
+++ src/services/bread.js
@@ -0,0 +1,32 @@
+export const breadProducts = [
+    {
+      id: 10,
+      name: "The Cheesy One - 70 gram",
+      price: 15,
+      photo: "/img/image15.png",
+    },
+    {
+      id: 11,
+      name: "Hold Back Vampire - 70 gram",
+      price: 15,
+      photo: "/img/image16.png"
+    },
+    {
+      id: 12,
+      name: "Going nuts - 70 gram",
+      price: 15,
+      photo: "/img/image17.png"
+    },
+    {
+      id: 13,
+      name: "Love in Lavain - delicious organic bread - 70 gram",
+      price: 15,
+      photo: "/img/image18.png"
+    },
+    {
+      id: 14,
+      name: "As Good As It Baguettes - Organic - 70 gram",
+      price: 15,
+      photo: "/img/image23.png"
+    },
+  ];
