761fa1dac967c3a45f6da9e4644f0ec5dd28f357
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-10-22 23:15:47 +0200

    Create soup.js

---
+++ src/services/soup.js
@@ -0,0 +1,32 @@
+export const soupProducts = [
+  {
+    id: 1,
+    name: "The Healing - (Butternut squash, red onions, garlic, coconut milk)",
+    price: 50,
+    photo: "/img/image9.png",
+  },
+  {
+    id: 2,
+    name: "So Long Soup - (Parsnips, rosted garlic, lemon rosmary, maple syrup)",
+    price: 50,
+    photo: "/img/image10.png"
+  },
+  {
+    id: 3,
+    name: "Chilli Billie - (Carrots, chilli flakes, sweet potato, coconut milk )",
+    price: 50,
+    photo: "/img/image6.png"
+  },
+  {
+    id: 4,
+    name: "Miss Muscle - (Green peas, coriander, green chilli, lime, coconut milk)",
+    price: 50,
+    photo: "/img/image7.png"
+  },
+  {
+    id: 5,
+    name: "Low Carb - (Sweet potato, ginger, cummin marsala, garlic, coconut milk)",
+    price: 50,
+    photo: "/img/image8.png"
+  },
+];
