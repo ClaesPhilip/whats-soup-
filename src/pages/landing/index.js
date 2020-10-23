d94fb1c26a97a97e099bf2c85c6efb05f3266a08
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-10-23 23:01:14 +0200

    Update index.js

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -5,22 +5,16 @@ import './landing.css';
 
 const LandingPage = () => {
 
-
-
     return(
-        
         <div className='landing-container'>
-            <h1 className='landing-heads'>Eey!</h1>
+            <p className='landing-heads'>Eey!</p>
             <h3 className='landing-head'>what´s soup?</h3>
 
             <Link to="/store" className="landingBtn">Go to store</Link>
             <Link to="/store" className="landingBtn">Log in</Link>
             <Link to="/store" className="landingBtn">Register</Link>
-
         </div>
-        
     );
 }
-
 
 export default LandingPage;
